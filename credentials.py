d8fe8d8f0494bdacea0d84d1136d55307e67ef89
sirrotich <rotichtitus12@gmail.com>
2019-04-07 16:53:16 +0300

    add a function to save credentials

--- credentials.py
+++ credentials.py
@@ -4,6 +4,7 @@ class Credentials:
     """
     credentials_list = [] #empty credentials list
 
+
     def __init__(self,credentials_name,usr_name,password):
         self.credentials_name = credentials_name
         self.usr_name = usr_name
@@ -17,44 +18,44 @@ class Credentials:
 
         Credentials.credentials_list.append(self)    
 
-    @classmethod
-    def delete_credentials(self):
+    # @classmethod
+    # def delete_credentials(self):
 
-        '''
-        delete_credentials method deletes a saved credentials from the credentials_list
-        '''
-        for credentials in cls.credentials_list:
-            if credentials.credentials_name == name:
-                return credentials_list.remove(self)  
-
-
-    @classmethod
-    def find_by_name(cls,name):
-        for credentials in cls.credentials_list:
-            if credentials.credentials_name == name:
-                return credentials  
+    #     '''
+    #     delete_credentials method deletes a saved credentials from the credentials_list
+    #     '''
+    #     for credentials in cls.credentials_list:
+    #         if credentials.credentials_name == name:
+    #             return credentials_list.remove(self)  
 
 
-    @classmethod
-    def credentials_exist(cls,name):
-        '''
-        Method that checks if a credentials exists from the credentials list.
-        Args:
-            name: Acc name to search if it exists
-        Returns :
-            Boolean: True or false depending if the credentials exists
-        '''
-        for credentials in cls.credentials_list:
-            if credentials.password == name:
-                    return credentials
-
-        return False      
+    # @classmethod
+    # def find_by_name(cls,name):
+    #     for credentials in cls.credentials_list:
+    #         if credentials.credentials_name == name:
+    #             return credentials  
 
 
-    @classmethod
-    def display_credentials(cls):  #check this line later
-        '''
-        method that returns the credentials list
-        '''
-        return cls.credentials_list            
+    # @classmethod
+    # def credentials_exist(cls,name):
+    #     '''
+    #     Method that checks if a credentials exists from the credentials list.
+    #     Args:
+    #         name: Acc name to search if it exists
+    #     Returns :
+    #         Boolean: True or false depending if the credentials exists
+    #     '''
+    #     for credentials in cls.credentials_list:
+    #         if credentials.password == name:
+    #                 return credentials
+
+    #     return False      
+
+
+    # @classmethod
+    # def display_credentials(cls):  #check this line later
+    #     '''
+    #     method that returns the credentials list
+    #     '''
+    #     return cls.credentials_list            
                       
